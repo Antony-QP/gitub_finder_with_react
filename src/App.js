13d65739a7c06bb1092fd0505b29f3d05bde47af
Antony Parker <antonyqparker@gmail.com>
2020-10-07 14:51:48 +0100

    created user component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from "react";
 import Navbar from "./components/layout/Navbar.js";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends Component {
   render() {
-    const numbers = [1, 2, 3];
     return (
       <div className='App'>
-        <Navbar title={numbers} />
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
