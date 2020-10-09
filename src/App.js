b4a4c43888a85741b5437ebbcd7f76544b045f01
Antony Parker <antonyqparker@gmail.com>
2020-10-09 11:54:11 +0100

    user repos are now displayed and app is fully functioning

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -40,6 +41,15 @@ class App extends Component {
     this.setState({user: res.data, loading: false});
   };
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({repos: res.data, loading: false});
+  };
   // clearUsers from state
   clearUsers = () => {
     this.setState({users: [], loading: false});
@@ -54,7 +64,7 @@ class App extends Component {
 
   render() {
     // Destructure here
-    const {users, loading} = this.state;
+    const {users, loading, repos} = this.state;
     return (
       <Router>
         <div className='App'>
@@ -87,7 +97,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={this.state.user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
